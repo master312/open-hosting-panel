1cdca0ebaa54f3ca4fba3478bb2683ae415acd1f
master312 <sythinian@gmail.com>
2019-03-06 22:28:19 +0100

    Basic backend api hello world

--- backend/server.js
+++ backend/server.js
@@ -2,18 +2,10 @@ const express = require('express');
 
 const app = express();
 
-app.get('/api/hello_wolrd_test', (req, res) => {
-  const demoData = [
-    {id: 1, data1: 'like WAAT?', subData: 'I AM GOD'},
-    {id: 2, data1: 'Jesus is that you?', subData: 'Nope'},
-    {id: 2, data1: 'Can you hear me???', subData: 'YEEES, FUCKING YeeeeeeeS'},
-  ];
-
-
-  console.log("Got hello_wolrd_test request and responded with dataaaaaaa");
-  res.json(demoData);
-});
-
 const port = 5000;
+
+
+app.use('/api', require('./api/main'));
+
 
 app.listen(port, () => 'Server running on port ${port}');

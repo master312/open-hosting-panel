c37db335221c33896a6bfbb58d83678367a6772d
Petar Ostojic <petar.ostojic@pannonia-expertise.net>
2019-03-08 16:14:10 +0100

    Created authorization STUB.

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,37 @@
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 
 const app = express();
 
 const port = 5000;
 
-app.use(bodyParser.urlencoded({
-  extended: true
-}))
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
-app.use(bodyParser.json())
+function useAuthenticatedRouter(path, router) {
+  var authenticationHandler = function (req, res, next) {
+    var authHeader = req.get('Authorization');
+    if (!authHeader) {
+      /* Missing header */
+      res.status(401);
+      res.send();
+      return;
+    }
+    if (authHeader != "Bearer 8d090338-935b-4afd-9c52-5792c1a74701") {
+      /* Invalid token */
+      res.status(401);
+      res.send('Invalid token');
+      return;
+    }
 
+    next();
+  }
+  
+  app.use(path, authenticationHandler);
+  app.use(path, router);
+}
 
-app.use('/api', require('./api/api'));
+useAuthenticatedRouter('/api', require('./api/api'));
 
 
 app.listen(port, () => 'Server running on port ${port}');
